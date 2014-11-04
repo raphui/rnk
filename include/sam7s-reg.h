94ec017b72a02d79a87c23c0fb2738044955217b
Raphaël Poggi <poggi.raph@gmail.com>
2014-11-04 11:56:38 -0800

    sam7: update reg file and link script

--- include/sam7s-reg.h
+++ include/sam7s-reg.h
@@ -1,280 +1,59 @@
-/* ----------------------------------------------------------------------------
- *         ATMEL Microcontroller Software Support 
- * ----------------------------------------------------------------------------
- * Copyright (c) 2008, Atmel Corporation
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the disclaimer below.
- *
- * Atmel's name may not be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
- * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * ----------------------------------------------------------------------------
- */
+//  ----------------------------------------------------------------------------
+//          ATMEL Microcontroller Software Support  -  ROUSSET  -
+//  ----------------------------------------------------------------------------
+//  Copyright (c) 2006, Atmel Corporation
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  - Redistributions of source code must retain the above copyright notice,
+//  this list of conditions and the disclaimer below.
+//
+//  Atmel's name may not be used to endorse or promote products derived from
+//  this software without specific prior written permission.
+//
+//  DISCLAIMER:  THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+//  DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  ----------------------------------------------------------------------------
+// File Name           : AT91SAM7S256.h
+// Object              : AT91SAM7S256 definitions
+// Generated           : AT91 SW Application Group  07/07/2008 (16:12:57)
+//
+// CVS Reference       : /AT91SAM7S256.pl/1.12/Wed Aug 30 14:08:39 2006//
+// CVS Reference       : /SYS_SAM7S.pl/1.2/Thu Feb  3 10:47:39 2005//
+// CVS Reference       : /MC_SAM7S.pl/1.4/Thu Feb 16 16:45:50 2006//
+// CVS Reference       : /PMC_SAM7S_USB.pl/1.4/Tue Feb  8 14:00:19 2005//
+// CVS Reference       : /RSTC_SAM7S.pl/1.2/Wed Jul 13 15:25:17 2005//
+// CVS Reference       : /UDP_4ept.pl/1.1/Thu Aug  3 12:26:00 2006//
+// CVS Reference       : /PWM_SAM7S.pl/1.1/Tue May 10 12:38:54 2005//
+// CVS Reference       : /RTTC_6081A.pl/1.2/Thu Nov  4 13:57:22 2004//
+// CVS Reference       : /PITC_6079A.pl/1.2/Thu Nov  4 13:56:22 2004//
+// CVS Reference       : /WDTC_6080A.pl/1.3/Thu Nov  4 13:58:52 2004//
+// CVS Reference       : /VREG_6085B.pl/1.1/Tue Feb  1 16:40:38 2005//
+// CVS Reference       : /AIC_6075B.pl/1.3/Fri May 20 14:21:42 2005//
+// CVS Reference       : /PIO_6057A.pl/1.2/Thu Feb  3 10:29:42 2005//
+// CVS Reference       : /DBGU_6059D.pl/1.1/Mon Jan 31 13:54:41 2005//
+// CVS Reference       : /US_6089C.pl/1.1/Mon Jan 31 13:56:02 2005//
+// CVS Reference       : /SPI_6088D.pl/1.3/Fri May 20 14:23:02 2005//
+// CVS Reference       : /SSC_6078A.pl/1.1/Tue Jul 13 07:10:41 2004//
+// CVS Reference       : /TC_6082A.pl/1.7/Wed Mar  9 16:31:51 2005//
+// CVS Reference       : /TWI_6061A.pl/1.2/Fri Oct 27 11:40:48 2006//
+// CVS Reference       : /PDC_6074C.pl/1.2/Thu Feb  3 09:02:11 2005//
+// CVS Reference       : /ADC_6051C.pl/1.1/Mon Jan 31 13:12:40 2005//
+//  ----------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-/// \unit
-/// !Purpose
-/// 
-/// Definition of AT91SAM7S-EK characteristics, AT91SAM7S-dependant PIOs and
-/// external components interfacing.
-/// 
-/// !Contents
-/// This file provide a large number of definitions, which are of three
-/// different types.
-///
-/// PIO definitions are prefixed with #PIN_# or #PINS_#. They are to be used
-/// with the pio peripheral to configure the pins required by the application.
-///
-/// First, additional information about the platform is provided by several
-/// constants:
-///    - BOARD_NAME is a string containing the board name
-///    - The chip family and board (at91sam7s and at91sam7sek) are also
-///      provided.
-///    - BOARD_MAINOSC and BOARD_MCK contains the standard frequency for the
-///      main oscillator and the master clock.
-///
-/// Contants prefixed with #BOARD_USB_# give information about the USB device
-/// peripheral that is provided in the chip.
-///
-/// Defines prefixed with #PIN_# contain only one pin (and thus can be safely
-/// used to initialize a single Pin instance), whereas defines starting with
-/// #PINS_# contains either a single Pin instance with multiple pins inside it,
-/// or a list of several Pin instances; they must be used as Pin[] array
-/// initializer values, otherwise they are not safe.
-///
-/// Finally, some information about the flash controller is given by definitions
-/// prefixed with #BOARD_FLASH_#.
-//------------------------------------------------------------------------------
-
-#ifndef BOARD_H 
-#define BOARD_H
-
-//------------------------------------------------------------------------------
-//         Global Definitions
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Board
-//------------------------------------------------------------------------------
-/// String containing the name of the board.
-#define BOARD_NAME      "AT91SAM7S-EK"
-/// Board definition.
-#define at91sam7sek
-/// Family definition.
-#define at91sam7s
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Clocks
-//------------------------------------------------------------------------------
-/// Frequency of the board main oscillator, in Hz.
-#define BOARD_MAINOSC           18432000
-
-/// Master clock frequency (when using board_lowlevel.c), in Hz.
-#define BOARD_MCK               48000000
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// ADC
-//------------------------------------------------------------------------------
-/// ADC clock frequency, at 10-bit resolution (in Hz)
-#define ADC_MAX_CK_10BIT         5000000
-/// ADC clock frequency, at 8-bit resolution (in Hz)
-#define ADC_MAX_CK_8BIT          8000000
-/// Startup time max, return from Idle mode (in µs)
-#define ADC_STARTUP_TIME_MAX       20
-/// Track and hold Acquisition Time min (in ns)
-#define ADC_TRACK_HOLD_TIME_MIN   600
-
-//------------------------------------------------------------------------------
-// USB
-//------------------------------------------------------------------------------
-/// Indicates the chip has a UDP controller.
-#define BOARD_USB_UDP
-
-/// Indicates the D+ pull-up is externally controlled.
-#define BOARD_USB_PULLUP_EXTERNAL
-
-/// Number of endpoints in the USB controller.
-#define BOARD_USB_NUMENDPOINTS                  4
-
-/// Returns the maximum packet size of the given endpoint.
-/// \param i  Endpoint number.
-/// \return Maximum packet size in bytes of endpoint. 
-#define BOARD_USB_ENDPOINTS_MAXPACKETSIZE(i)    ((i == 0) ? 8 : 64)
-
-/// Returns the number of FIFO banks for the given endpoint.
-/// \param i  Endpoint number.
-/// \return Number of FIFO banks for the endpoint.
-#define BOARD_USB_ENDPOINTS_BANKS(i)            (((i == 0) || (i == 3)) ? 1 : 2)
-
-/// USB attributes configuration descriptor (bus or self powered, remote wakeup)
-#define BOARD_USB_BMATTRIBUTES                  USBConfigurationDescriptor_SELFPOWERED_NORWAKEUP
-//#define BOARD_USB_BMATTRIBUTES                  USBConfigurationDescriptor_BUSPOWERED_NORWAKEUP
-//#define BOARD_USB_BMATTRIBUTES                  USBConfigurationDescriptor_SELFPOWERED_RWAKEUP
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Pins
-//------------------------------------------------------------------------------
-/// DBGU pins definition. Contains DRXD (PA9) and DTXD (PA10).
-#define PINS_DBGU  {0x00000600, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-
-/// LED #0 pin definition (PA0).
-#define PIN_LED_DS1   {1 << 0, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT}
-/// LED #1 pin definition (PA1).
-#define PIN_LED_DS2   {1 << 1, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT}
-/// LED #2 pin definition (PA2).
-#define PIN_LED_DS3   {1 << 2, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT}
-/// LED #3 pin definition (PA3).
-#define PIN_LED_DS4   {1 << 3, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT}
-/// List of the four LED pin definitions (PA0, PA1, PA2 & PA3)
-#define PINS_LEDS   PIN_LED_DS1, PIN_LED_DS2, PIN_LED_DS3, PIN_LED_DS4
-
-/// Push button #0 definition (PA19).
-#define PIN_PUSHBUTTON_1    {1 << 19, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEGLITCH | PIO_PULLUP}
-/// Push button #1 definition (PA20).
-#define PIN_PUSHBUTTON_2    {1 << 20, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEGLITCH | PIO_PULLUP}
-/// Push button #2 definition (PA15).
-#define PIN_PUSHBUTTON_3    {1 << 15, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEGLITCH | PIO_PULLUP}
-/// Push button #3 definition (PA14).
-#define PIN_PUSHBUTTON_4    {1 << 14, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEGLITCH | PIO_PULLUP}
-/// List of all push button definitions (PA19, PA20, PA15 & PA14).
-#define PINS_PUSHBUTTONS    PIN_PUSHBUTTON_1, PIN_PUSHBUTTON_2, PIN_PUSHBUTTON_3, PIN_PUSHBUTTON_4
-/// Push button #1 index.
-#define PUSHBUTTON_BP1      0
-/// Push button #2 index.
-#define PUSHBUTTON_BP2      1
-/// Push button #1 index.
-#define PUSHBUTTON_BP3      2
-/// Push button #2 index.
-#define PUSHBUTTON_BP4      3
-/// Simulated joystick LEFT index.
-#define JOYSTICK_LEFT       0
-/// Simulated joystick RIGHT index.
-#define JOYSTICK_RIGHT      1
-
-/// USART0 TXD pin definition (PA5).
-#define PIN_USART0_RXD  {1 << 5, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// USART0 RXD pin definition (PA6).
-#define PIN_USART0_TXD  {1 << 6, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// USART0 RTS pin definition.
-#define PIN_USART0_RTS  {1 << 7, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// USART0 CTS pin definition.
-#define PIN_USART0_CTS  {1 << 8, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// USART0 SCK pin definition.
-#define PIN_USART0_SCK  {1 << 2, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_B, PIO_DEFAULT}
-
-/// SPI MISO pin definition (PA12).
-#define PIN_SPI_MISO   {1 << 12, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_PULLUP}
-/// SPI MOSI pin definition (PA13).
-#define PIN_SPI_MOSI   {1 << 13, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// SPI SPCK pin definition (PA14).
-#define PIN_SPI_SPCK   {1 << 14, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// SPI pins definition. Contains MISO, MOSI & SPCK (PA12, PA13 & PA14).
-#define PINS_SPI       PIN_SPI_MISO, PIN_SPI_MOSI, PIN_SPI_SPCK
-/// SPI chip select 0 pin definition (PA11).
-#define PIN_SPI_NPCS0  {1 << 11, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// SPI chip select 1
-#define PIN_SPI_NPCS1  {1 << 31, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// SPI chip select 2
-#define PIN_SPI_NPCS2  {1 << 10, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_B, PIO_DEFAULT}
-/// SPI chip select 3
-#define PIN_SPI_NPCS3  {1 <<  3, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_B, PIO_DEFAULT}
-
-/// PWMC PWM0 pin definition (PA0).
-#define PIN_PWMC_PWM0  {1 << 0, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// PWMC PWM0 pin definition (PA1).
-#define PIN_PWMC_PWM1  {1 << 1, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-/// PWMC PWM0 pin definition (PA2).
-#define PIN_PWMC_PWM2  {1 << 2, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-
-/// PWM pin definition for LED0
-#define PIN_PWM_LED0 PIN_PWMC_PWM1
-/// PWM pin definition for LED1
-#define PIN_PWM_LED1 PIN_PWMC_PWM2
-/// PWM channel for LED0
-#define CHANNEL_PWM_LED0 1
-/// PWM channel for LED1
-#define CHANNEL_PWM_LED1 2
-
-/// TWI pins definition. Contains TWD (PA3) and TWCK (PA4).
-#define PINS_TWI  {0x00000018, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
-
-/// ADC_AD0 pin definition.
-#define PIN_ADC_AD0 {1 << 17, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEFAULT}
-/// ADC_AD1 pin definition.
-#define PIN_ADC_AD1 {1 << 18, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEFAULT}
-/// ADC_AD2 pin definition. (mixed with PIN_PUSHBUTTON_1)
-#define PIN_ADC_AD2 {1 << 19, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEFAULT}
-/// ADC_AD3 pin definition. (mixed with PIN_PUSHBUTTON_2)
-#define PIN_ADC_AD3 {1 << 20, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEFAULT}
-/// Pins ADC
-#define PINS_ADC PIN_ADC_AD0, PIN_ADC_AD1, PIN_ADC_AD2, PIN_ADC_AD3
-
-/// USB VBus monitoring pin definition (PA13).
-#define PIN_USB_VBUS    {1 << 13, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEFAULT}
-/// USB pull-up control pin definition (PA16).
-#define PIN_USB_PULLUP  {1 << 16, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT}
-//------------------------------------------------------------------------------
-
-/// !SD Card SPI
-/// - BOARD_SD_SPI_BASE
-/// - BOARD_SD_SPI_ID  
-/// - BOARD_SD_SPI_PINS
-/// - BOARD_SD_NPCS    
-
-/// Not define in our board, but customer can add this feature
-/// Base address of the SPI peripheral connected to the SD card.
-#define BOARD_SD_SPI_BASE   AT91C_BASE_SPI
-/// Identifier of the SPI peripheral connected to the SD card.
-#define BOARD_SD_SPI_ID     AT91C_ID_SPI
-/// List of pins to configure to access the SD card
-#define BOARD_SD_SPI_PINS   PINS_SPI, PIN_SPI_NPCS1
-/// NPCS number
-#define BOARD_SD_NPCS       1
-
-//------------------------------------------------------------------------------
-// Flash
-//------------------------------------------------------------------------------
-/// Indicates chip has an EFC.
-#define BOARD_FLASH_EFC
-/// Address of the IAP function in ROM.
-#define BOARD_FLASH_IAP_ADDRESS         0x300E08
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-/// \page "SAM7S-EK - External components"
-/// This page lists the definitions related to external on-board components
-/// located in the board.h file for the SAM7S-EK.
-/// 
-/// !ISO7816
-/// - PIN_SMARTCARD_CONNECT
-/// - PIN_ISO7816_RSTMC
-/// - PINS_ISO7816
-
-/// Smartcard detection pin
-#define PIN_SMARTCARD_CONNECT   {1 << 5, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_INPUT, PIO_DEFAULT}
-/// PIN used for reset the smartcard
-#define PIN_ISO7816_RSTMC       {1 << 7, AT91C_BASE_PIOA, AT91C_ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
-/// Pins used for connect the smartcard
-#define PINS_ISO7816            PIN_USART0_TXD, PIN_USART0_SCK, PIN_ISO7816_RSTMC
+#ifndef AT91SAM7S256_H
+#define AT91SAM7S256_H
 
 #ifndef __ASSEMBLY__
 typedef volatile unsigned int AT91_REG;// Hardware register definition
@@ -2447,5 +2226,4 @@ typedef struct _AT91S_UDP {
 #define AT91C_IFLASH_NB_OF_PAGES	 (1024) // Internal FLASH Number of Pages: 1024 bytes
 #define AT91C_IFLASH_NB_OF_LOCK_BITS	 (16) // Internal FLASH Number of Lock Bits: 16 bytes
 
-#endif //#ifndef BOARD_H
-
+#endif
