2e9b9ca7af53a8120d3c1a3de8f6a3b326b2d368
Raphaël Poggi <poggi.raph@gmail.com>
2014-11-19 11:30:16 +0100

    scheduler: debug print

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -1,5 +1,5 @@
-#include <stdint.h>
 #include <scheduler.h>
+#include <io.h>
 
 
 /* At the moment it's a round-robin schedule */
@@ -7,19 +7,20 @@
 void schedule(void)
 {
 	int i;
+	printk("abc\r\n");
 	//	long quantum = 150;	/* in ms */
 
-	while (1) {
-		i = NR_TASK - 1;
-
-		current_task = task[i];
-
-		while (i--) {
-			if (current_task->state == TASK_RUNNING) {
-
-			}
-		}
-
-	}
+//	while (1) {
+//		i = NR_TASK - 1;
+//
+//		current_task = task[i];
+//
+//		while (i--) {
+//			if (current_task->state == TASK_RUNNING) {
+//
+//			}
+//		}
+//
+//	}
 
 }
