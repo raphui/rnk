d1c2fc54f4d3c7e165fd271ac786db7a604285f9
Raphaël Poggi <poggi.raph@gmail.com>
2014-11-19 12:46:14 -0800

    kernel: task: create context on adding, add debug trace

--- kernel/task.c
+++ kernel/task.c
@@ -1,4 +1,6 @@
 #include <task.h>
+#include <scheduler.h>
+#include <io.h>
 
 static struct task *current_task = NULL;
 
@@ -9,11 +11,15 @@ void add_task(void (*func)(void), unsigned int priority)
 	task[task_count].start_stack = TASK_STACK_START + (task_count * TASK_STACK_OFFSET);
 	task[task_count].func = func;
 
+	/* Creating task context */
+	create_context(task[task_count]);
+
 	task_count++;
 }
 
 void switch_task(struct task _task)
 {
+	printk("#\r\n");
 	if (current_task) {
 		current_task->state = TASK_STOPPED;
 		_task.state = TASK_RUNNING;
