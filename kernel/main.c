ac670792916f14f4655d6a2772c89505f7c739cd
Raphaël Poggi <poggi.raph@gmail.com>
2014-11-19 16:28:35 +0100

    kernel: cleanup main

--- kernel/main.c
+++ kernel/main.c
@@ -8,15 +8,9 @@ extern int get_value_cp(void);
 
 int main(void)
 {
-
-//	low_level_init();
-
 	uart_init();
 
-//	while(1)
-		printk("\r\nHello World from RNK ( Raphio new kernel )\r\n");
-
-//	printk("CP15: %x\r\n" , get_value_cp());
+	printk("\r\nHello World from RNK ( Raphio new kernel )\r\n");
 
 	while(1)
 		;
