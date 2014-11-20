769177b442b11ddb121ed27314d182da46aac94b
Raphaël Poggi <poggi.raph@gmail.com>
2014-11-20 17:04:23 +0100

    arm: kernel: implement svc_create_context

--- arch/arm/kernel/svc.h
+++ arch/arm/kernel/svc.h
@@ -1,6 +1,7 @@
 #ifndef SVC_H
 #define SVC_H
 
+void svc_create_context(unsigned int sp, unsigned int func, unsigned int end, unsigned int fp);
 void svc_activate_context(unsigned int sp);
 void svc_switch_context(unsigned int sp);
 
