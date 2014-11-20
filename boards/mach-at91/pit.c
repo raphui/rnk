b10cc8651d253103751473864a688afde7dbc8c3
Raphaël Poggi <poggi.raph@gmail.com>
2014-11-20 11:41:59 +0100

    mach-at91: pit: fix bug in enable pit

--- boards/mach-at91/pit.c
+++ boards/mach-at91/pit.c
@@ -13,7 +13,10 @@ static void pit_init(unsigned int period, unsigned int pit_frequency)
 
 static void pit_enable(void)
 {
-	writel(AT91C_BASE_PITC + PITC_PIMR, AT91C_PITC_PITEN);
+	unsigned int tmp = 0;
+	tmp = readl(AT91C_BASE_PITC + PITC_PIMR);
+	tmp |= AT91C_PITC_PITEN;
+	writel(AT91C_BASE_PITC + PITC_PIMR, tmp);
 }
 
 static void pit_enable_it(void)
