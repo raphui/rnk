939fed729b9a40b6ef01afb86c7b6e4dd7b9ebd8
Raphaël Poggi <poggi.raph@gmail.com>
2015-01-06 14:47:55 +0100

    arm: kernel: use new include

--- arch/arm/kernel/svc.c
+++ arch/arm/kernel/svc.c
@@ -16,31 +16,11 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <task.h>
-#include <scheduler.h>
+#include <stdio.h>
+
 #include <arch/svc.h>
 
-void create_context(struct registers *_regs, struct task _task)
+void svc_handler(unsigned int call)
 {
-	svc_create_context(_regs, _task.start_stack, (unsigned int)_task.func, (unsigned int)end_task);
-}
-
-void activate_context(struct task _task)
-{
-	svc_activate_context(_task.regs);
-}
-
-void switch_context(struct registers *_current_regs, struct registers *_task_regs)
-{
-	svc_switch_context(_current_regs, _task_regs);
-}
-
-void save_user_context(void)
-{
-	svc_save_user_context();
-}
-
-void get_user_context(void)
-{
-	svc_get_user_context();
+	printk("svc_handler: got call %d\r\n", call);
 }
