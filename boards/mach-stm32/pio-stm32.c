24843a21d7bfcc296c4bb7c32b7e7bc02689270a
Raphael Poggi <poggi.raph@gmail.com>
2015-04-30 02:09:55 -0700

    mach-stm32: pio: implement output, input and alternate functions

--- boards/mach-stm32/pio-stm32.c
+++ boards/mach-stm32/pio-stm32.c
@@ -18,16 +18,41 @@
 #include <board.h>
 #include <utils.h>
 
+/* TODO: Output mode opendrain / push-pull */
+
+#define GPIO_MODER(pin)			GPIO_MODER_MODER#pin
+#define GPIO_MODER_OUTPUT(pin)		GPIO_MODER_MODER#pin_0
+#define GPIO_MODER_ALTERNATE(pin)	GPIO_MODER_MODER#pin_1
+
+#define GPIO_PUPDR(pin)			GPIO_PUPDR_PUPDR#pin
+#define GPIO_PUPDR_PULLUP(pin)		GPIO_PUPDR_PUPDR#pin_0
+
 static void pio_set_output(unsigned int port, unsigned int mask, int pull_up)
 {
+	port->MODER = GPIO_MODER_OUTPUT(mask);
+
+	if (pull_up)
+		port->PUPDR = GPIO_PUPDR_PULLUP(mask);
+	else
+		port->PUPDR &= ~(GPIO_PUPDR(mask));
 }
 
 static void pio_set_input(unsigned int port, unsigned int mask, int pull_up, int filter)
 {
+	port->MODER &= ~(GPIO_MODER(mask));
 }
 
 static void pio_set_alternate(unsigned int port, unsigned int mask, unsigned int num)
 {
+	unsigned int afr_high_base = 8;
+	unsigned int afr_low_base = 0;
+
+	port->MODER = GPIO_MODER_ALTERNATE(mask);
+
+	if (pin > 7)
+		port->AFR[1] |= (num << ((mask - afr_high_base) * 4)
+	else
+		port->AFR[0] |= (num << (mask * 4))
 }
 
 static void pio_set_value(unsigned int port, unsigned int mask)
