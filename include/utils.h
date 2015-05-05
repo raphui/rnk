16c397843b0f496da6f817387bd623ffdb7de695
Raphael Poggi <poggi.raph@gmail.com>
2015-05-05 01:15:10 -0700

    include: utils: use __builtin_offsetof

--- include/utils.h
+++ include/utils.h
@@ -45,8 +45,8 @@ unsigned int readl(unsigned int reg);
  * @member:     the name of the member within the struct.
  *
  */
-#define container_of(ptr, type, member) ({                      \
-        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-        (type *)( (char *)__mptr - offsetof(type,member) );})
+#define container_of(ptr, type, member) ({ \
+    const typeof( ((type *)0)->member ) *__mptr = (ptr); \
+    (type *)( (char *)__mptr - __builtin_offsetof(type,member) );})
 
 #endif /* UTILS_H */
