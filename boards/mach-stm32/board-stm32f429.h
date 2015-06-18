7b72231fb87c47e0e35b1ecb862a5447f2add65f
Raphael Poggi <poggi.raph@gmail.com>
2015-06-18 09:59:55 -0700

    mach-stm32: stm32f429: change clock settings

--- boards/mach-stm32/board-stm32f429.h
+++ boards/mach-stm32/board-stm32f429.h
@@ -31,18 +31,18 @@
 /* SYSCLK = PLL_VCO / PLL_P =======> 168000000*/
 /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N  =======> 336000000 */
 
-#define SYSCLK		180000000
-#define HCLK		180000000
+#define SYSCLK		168000000
+#define HCLK		168000000
 #define AHB_PRES	1
 #define APB1_PRES	4
 #define APB2_PRES	2
-#define HSE		25000000
+#define HSE		8000000
 
 /*#define VECT_TAB_SRAM */
 #define VECT_TAB_OFFSET 0x00
-#define PLL_M		25
+#define PLL_M		8
 #define PLL_Q		7
-#define PLL_N		360
+#define PLL_N		336
 #define PLL_P		2
 #define HSE_STARTUP_TIMEOUT    0x05000
 #define RESET		0
