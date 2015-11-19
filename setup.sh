0f1ac442dba27000ad217f7a781bdcd91be82ab4
Raphael Poggi <poggi.raph@gmail.com>
2015-11-19 21:48:18 +0100

    setup.sh: export ARCH

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ export PATH=$PATH:/opt/cross/x-tools/arm-unknown-eabi/bin
 export CROSS_COMPILE=arm-unknown-eabi-
 export TOOLCHAIN_DIR=/opt/cross/x-tools/arm-unknown-eabi/
 export KERNEL_BASE=$(pwd)
+export ARCH=arm
