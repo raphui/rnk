eec05bc1e2abd022fcf7ec4d3d8711b326e1903d
Raphael Poggi <poggi.raph@gmail.com>
2016-02-09 10:40:35 +0100

    tools: sym: avoid generation of useless symbols

--- tools/sym.sh
+++ tools/sym.sh
@@ -5,7 +5,7 @@ BAK=utils/symbols.c.bak
 
 make()
 {
-	LIST=$(nm -S kernel.elf | awk '{line=$3; $1="0x"$1; $2="0x"$2; if(NF==4){line=$4}else{$2="0"}; printf("{%s, %s, \"%s\"},\n", $1, $2, line)}')
+	LIST=$(nm -S kernel.elf | awk '{line=$3; $1="0x"$1; $2="0x"$2; if(NF==4){line=$4}else{$2="0"}; if (line != "$d" && line != "$t"){printf("{%s, %s, \"%s\"},\n", $1, $2, line)}}')
 	awk -v l="$LIST" '/Generate/ { print; print l; next }1' $FILE > $BAK
 	mv $BAK $FILE
 }
