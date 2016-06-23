32848c4f689630b6774d4b3d3c48fea5225c76e8
Raphael Poggi <poggi.raph@gmail.com>
2016-06-23 08:55:10 +0200

    setup.sh: change toolchain dir

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export PATH=$PATH:/opt/cross/x-tools/arm-unknown-eabi/bin
-export CROSS_COMPILE=arm-unknown-eabi-
-export TOOLCHAIN_DIR=/opt/cross/x-tools/arm-unknown-eabi/
+export PATH=$PATH:/opt/cross/x-tools/gcc-arm-none-eabi-4_9-2014q4/bin
+export CROSS_COMPILE=arm-none-eabi-
 export KERNEL_BASE=$(pwd)
 export ARCH=arm
