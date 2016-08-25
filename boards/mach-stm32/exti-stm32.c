c7ec51739ec4c45f2b993c70ac4b2e7be4d7e4a8
Raphael Poggi <poggi.raph@gmail.com>
2016-08-25 17:18:52 +0200

    mach-stm32: exti: use irq driver to implements irq callback

--- boards/mach-stm32/exti-stm32.c
+++ boards/mach-stm32/exti-stm32.c
@@ -20,7 +20,19 @@
 #include <utils.h>
 #include <stdio.h>
 #include <arch/nvic.h>
+#include <armv7m/vector.h>
 #include <errno.h>
+#include <list.h>
+#include <irq.h>
+#include <mm.h>
+
+struct action {
+	void (*irq_action)(void);
+	int irq;
+	TAILQ_ENTRY(action) next;
+};
+
+static TAILQ_HEAD(, action) action_list;
 
 static int nvic_array[15] = {
 	EXTI0_IRQn,
@@ -103,7 +115,74 @@ static int stm32_exti_get_nvic_number(unsigned int gpio_num)
 	return nvic;
 }
 
-int stm32_exti_init(unsigned int gpio_base, unsigned int gpio_num)
+static void stm32_exti_isr(void *arg)
+{
+	unsigned int irq = vector_current_irq();
+
+	switch (irq) {
+	case EXTI0_IRQn:
+		stm32_exti_action(0);
+		break;
+	case EXTI1_IRQn:
+		stm32_exti_action(1);
+		break;
+	case EXTI2_IRQn:
+		stm32_exti_action(2);
+		break;
+	case EXTI3_IRQn:
+		stm32_exti_action(3);
+		break;
+	case EXTI4_IRQn:
+		stm32_exti_action(4);
+		break;
+	case EXTI9_5_IRQn:
+		stm32_exti_action(EXTI->PR & 0x3E);
+		break;
+	case EXTI15_10_IRQn:
+		stm32_exti_action(EXTI->PR & 0xFC00);
+		break;
+	}
+}
+
+int stm32_exti_init(void)
+{
+	int ret = 0;
+
+	ret = irq_request(EXTI0_IRQn, &stm32_exti_isr, NULL);
+	if (ret < 0)
+		goto fail;
+
+	ret = irq_request(EXTI1_IRQn, &stm32_exti_isr, NULL);
+	if (ret < 0)
+		goto fail;
+
+	ret = irq_request(EXTI2_IRQn, &stm32_exti_isr, NULL);
+	if (ret < 0)
+		goto fail;
+
+	ret = irq_request(EXTI3_IRQn, &stm32_exti_isr, NULL);
+	if (ret < 0)
+		goto fail;
+
+	ret = irq_request(EXTI4_IRQn, &stm32_exti_isr, NULL);
+	if (ret < 0)
+		goto fail;
+
+	ret = irq_request(EXTI9_5_IRQn, &stm32_exti_isr, NULL);
+	if (ret < 0)
+		goto fail;
+
+	ret = irq_request(EXTI15_10_IRQn, &stm32_exti_isr, NULL);
+	if (ret < 0)
+		goto fail;
+
+	TAILQ_INIT(&action_list);
+
+fail:
+	return ret;
+}
+
+int stm32_exti_configure_line(unsigned int gpio_base, unsigned int gpio_num)
 {
 	unsigned char mask = stm32_exti_base_mask(gpio_base);
 	int ret = 0;
@@ -201,5 +280,67 @@ int stm32_exti_disable_rising(unsigned int gpio_base, unsigned int gpio_num)
 	nvic_clear_interrupt(nvic);
 	nvic_disable_interrupt(nvic);
 
+	return ret;
+}
+
+int stm32_exti_action(unsigned int line)
+{
+	int ret = 0;
+	struct action *action = NULL;
+	void (*hook)(void) = NULL;
+
+	TAILQ_FOREACH(action, &action_list, next)
+		if (action->irq == line)
+			break;
+
+	if (!action) {
+		error_printk("no action has been found for exti: %d\n", line);
+		return -ENOSYS;
+	}
+
+	hook = action->irq_action;
+
+	hook();
+
+	return ret;
+}
+
+int stm32_exti_request_irq(unsigned int gpio_base, unsigned int gpio_num, void (*handler)(void), int flags, void *arg)
+{
+	int ret = 0;
+	struct action *action = NULL;
+
+	ret = stm32_exti_configure_line(gpio_num, gpio_base);
+	if (ret < 0) {
+		error_printk("failed to init exti for gpio_base: %x gpio_num: %d\n", gpio_base, gpio_num);
+		goto fail;
+	}
+
+	if (flags & IRQF_RISING)
+		stm32_exti_enable_rising(gpio_base, gpio_num);
+	else if (flags & IRQF_FALLING)
+		stm32_exti_enable_falling(gpio_base, gpio_num);
+	else {
+		error_printk("wrong flags for gpio_base: %x gpio_num: %d\n", gpio_base, gpio_num);
+		ret = -EINVAL;
+		goto fail;
+	}
+
+	action = (struct action *)kmalloc(sizeof(struct action));
+	if (!action) {
+		error_printk("cannot allocate exti irq action\n");
+		ret = -ENOMEM;
+		goto fail;
+	}
+
+	action->irq_action = handler;
+	action->irq = gpio_num;
+
+	if (TAILQ_EMPTY(&action_list))
+		TAILQ_INSERT_HEAD(&action_list, action, next);
+	else
+		TAILQ_INSERT_TAIL(&action_list, action, next);
+
+fail:
 	return ret;
 }
--- boards/mach-stm32/include/exti-stm32.h
+++ boards/mach-stm32/include/exti-stm32.h
@@ -20,8 +20,10 @@
 #define EXTI_STM32_H
 
 extern void stm32_exti_init(unsigned int gpio_base, unsigned int gpio_num);
+extern int stm32_exti_configure_line(unsigned int gpio_base, unsigned int gpio_num);
 extern void stm32_exti_enable_falling(unsigned int gpio_base, unsigned int gpio_num);
 extern void stm32_exti_enable_rising(unsigned int gpio_base, unsigned int gpio_num);
+int stm32_exti_request_irq(unsigned int gpio_base, unsigned int gpio_num, void (*handler)(void), int flags, void *arg);
 extern void stm32_exti_disable_falling(unsigned int gpio_base, unsigned int gpio_num);
 extern void stm32_exti_disable_rising(unsigned int gpio_base, unsigned int gpio_num);
 
