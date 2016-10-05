93813fbd505972455ffa1561ab2997bf186c63d1
Raphael Poggi <poggi.raph@gmail.com>
2016-10-05 11:13:25 +0200

    utils: string: add some strings functions

--- include/string.h
+++ include/string.h
@@ -18,8 +18,14 @@
 #ifndef STRING_H
 #define STRING_H
 
+#include <stddef.h>
+
 void memcpy(void *dst, const void *src, unsigned int num);
 void *memset(void *s, int c, unsigned int count);
 int strcmp(const char *cs, const char *ct);
+void *memchr(const void *ptr, int value, size_t num);
+int memcmp(const void *ptr1, const void *ptr2, size_t num);
+void memmove(void *dst, const void *src, size_t n);
+size_t strlen(const char *s);
 
 #endif /* STRING_H */
--- utils/string.c
+++ utils/string.c
@@ -48,3 +48,59 @@ int strcmp(const char *cs, const char *ct)
 
 	return res;
 }
+
+void *memchr(const void *ptr, int value, size_t num)
+{
+    const unsigned char *p = ptr;
+
+    while (num--) {
+        if (*p == value) {
+            return (void *) p;
+        }
+        p++;
+    }
+
+    return NULL;
+}
+
+int memcmp(const void *ptr1, const void *ptr2, size_t num)
+{
+    const unsigned char *p1 = ptr1;
+    const unsigned char *p2 = ptr2;
+
+    while (num--) {
+        if (*p1 != *p2) {
+            if (*p1 > *p2) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        }
+
+        p1++;
+        p2++;
+    }
+
+    return 0;
+}
+
+void memmove(void *dst, const void *src, size_t n)
+{
+    const char *s = src;
+    char *d = dst;
+    if ((unsigned int *)s < (unsigned int *)d)
+        while(n--) d[n] = s[n];
+    else
+        while(n--) *d++ = *s++;
+}
+
+size_t strlen(const char *s)
+{
+    size_t len = 0;
+    while (*s++) {
+        len++;
+    }
+    return len;
+}
+
