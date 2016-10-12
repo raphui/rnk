8bb662bb825cded3d31f6480c8a1ff600b989288
Raphael Poggi <poggi.raph@gmail.com>
2016-10-12 20:23:17 +0200

    mach-stm32: stm32f407: remove set_sys_clk function, it is now handle by rcc driver

--- boards/mach-stm32/stm32f407.c
+++ boards/mach-stm32/stm32f407.c
@@ -31,78 +31,6 @@
 #include <irq.h>
 #endif /* CONFIG_IRQ_SUBSYS */
 
-void set_sys_clock(void)
-{
-	/******************************************************************************/
-	/*            PLL (clocked by HSE) used as System clock source                */
-	/******************************************************************************/
-	unsigned int StartUpCounter = 0, HSEStatus = 0;
-
-	/* Enable HSE */
-	RCC->CR |= ((unsigned int)RCC_CR_HSEON);
-
-	/* Wait till HSE is ready and if Time out is reached exit */
-	do
-	{
-		HSEStatus = RCC->CR & RCC_CR_HSERDY;
-		StartUpCounter++;
-	} while((HSEStatus == 0) && (StartUpCounter != HSE_STARTUP_TIMEOUT));
-
-	if ((RCC->CR & RCC_CR_HSERDY) != RESET)
-	{
-		HSEStatus = (unsigned int)0x01;
-	}
-	else
-	{
-		HSEStatus = (unsigned int)0x00;
-	}
-
-	if (HSEStatus == (unsigned int)0x01)
-	{
-		/* Select regulator voltage output Scale 1 mode */
-		RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-		PWR->CR |= PWR_CR_VOS;
-
-		/* HCLK = SYSCLK / 1*/
-		RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
-
-		/* PCLK2 = HCLK / 2*/
-		RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
-
-		/* PCLK1 = HCLK / 4*/
-		RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
-
-		/* Configure the main PLL */
-		RCC->PLLCFGR = PLL_M | (PLL_N << 6) | (((PLL_P >> 1) -1) << 16) |
-			(RCC_PLLCFGR_PLLSRC_HSE) | (PLL_Q << 24);
-
-		/* Enable the main PLL */
-		RCC->CR |= RCC_CR_PLLON;
-
-		/* Wait till the main PLL is ready */
-		while((RCC->CR & RCC_CR_PLLRDY) == 0)
-			;
-
-		/* Configure Flash prefetch, Instruction cache, Data cache and wait state */
-		FLASH->ACR = FLASH_ACR_PRFTEN | FLASH_ACR_ICEN |FLASH_ACR_DCEN |FLASH_ACR_LATENCY_5WS;
-
-
-		/* Select the main PLL as system clock source */
-		RCC->CFGR &= (unsigned int)((unsigned int)~(RCC_CFGR_SW));
-		RCC->CFGR |= RCC_CFGR_SW_PLL;
-
-		/* Wait till the main PLL is used as system clock source */
-		while ((RCC->CFGR & (unsigned int)RCC_CFGR_SWS ) != RCC_CFGR_SWS_PLL);
-			;
-	}
-	else
-	{ /* If HSE fails to start-up, the application will have wrong clock
-	     configuration. User can add here some code to deal with this error */
-		while(1)
-			;
-	}
-}
-
 void low_level_init(void)
 {
 	/* Reset RCC clock */
@@ -116,10 +44,6 @@ void low_level_init(void)
 	RCC->CR &= (unsigned int)0xFFFBFFFF;
 	/* Disable all interrupts */
 	RCC->CIR = 0x00000000;
-
-	/* Configure the System clock source, PLL Multiplier and Divider factors, 
-	   AHB/APBx prescalers and Flash settings ----------------------------------*/
-	set_sys_clock();
 
 	/* Configure the Vector Table location add offset address ------------------*/
 #ifdef VECT_TAB_SRAM
