f0e1fdd593e38dfcc863eff28c8ba5887944e0b8
Raphael Poggi <poggi.raph@gmail.com>
2016-10-13 20:49:11 +0200

    drivers: usart: redesign subsystem

--- drivers/usart.c
+++ drivers/usart.c
@@ -22,14 +22,17 @@
 #include <mm.h>
 #include <errno.h>
 #include <string.h>
+#include <init.h>
 
-static struct usart *usart;
 static int dev_count = 0;
 static char dev_prefix[10] = "/dev/tty";
+static struct list_node usart_device_list;
+static struct list_node usart_master_list;
+
 
 static int usart_read(struct device *dev, unsigned char *buff, unsigned int size)
 {
-	struct usart *usart = container_of(dev, struct usart, dev);
+	struct usart_master *usart = container_of(dev, struct usart_master, dev);
 
 	verbose_printk("reading from usart !\n");
 
@@ -38,58 +41,160 @@ static int usart_read(struct device *dev, unsigned char *buff, unsigned int size
 
 static int usart_write(struct device *dev, unsigned char *buff, unsigned int size)
 {
-	struct usart *usart = container_of(dev, struct usart, dev);
+	struct usart_master *usart = container_of(dev, struct usart_master, dev);
 
 	verbose_printk("writing from usart !\n");
 
 	return usart_ops.write(usart, buff, size);
 }
 
-int usart_init(unsigned int num, unsigned int base_reg, unsigned int baud_rate)
+struct usart_device *usart_new_device(void)
+{
+	struct usart_device *usartdev = NULL;
+
+	usartdev = (struct usart_device *)kmalloc(sizeof(struct usart_device));
+	if (!usartdev) {
+		error_printk("cannot allocate usart device\n");
+		return NULL;
+	}
+
+	dev_count++;
+
+	return usartdev;
+}
+
+int usart_remove_device(struct usart_device *usart)
 {
 	int ret = 0;
+	struct usart_device *usartdev = NULL;
 
-	usart = (struct usart *)kmalloc(sizeof(*usart));
+	list_for_every_entry(&usart_device_list, usartdev, struct usart_device, node)
+		if (usartdev == usart)
+			break;
+
+	if (usartdev) {
+		list_delete(&usartdev->node);
+		kfree(usartdev);
+	}
+	else
+		ret = -ENOENT;
+
+
+	dev_count--;
+
+	return ret;
+}
+
+int usart_register_device(struct usart_device *usart)
+{
+	char tmp[10] = {0};
+
+	memcpy(tmp, dev_prefix, sizeof(dev_prefix));
+	
+	/* XXX: ascii 0 start at 0x30 */
+	tmp[8] = 0x30 + usart->master->num;
+
+	memcpy(usart->dev.name, tmp, sizeof(tmp));
+
+	list_add_tail(&usart_device_list, &usart->node);
+
+	return 0;
+}
+
+struct usart_master *usart_new_master(void)
+{
+	struct usart_master *usart = NULL;
+
+	usart = (struct usart_master *)kmalloc(sizeof(struct usart_master));
 	if (!usart) {
-		error_printk("cannot allocate usart\r\n");
+		error_printk("cannot allocate usart master\n");
+		return NULL;
+	}
+
+	return usart;
+}
+
+int usart_remove_master(struct usart_master *usart)
+{
+	int ret = 0;
+	struct usart_master *usartdev = NULL;
+
+	list_for_every_entry(&usart_master_list, usartdev, struct usart_master, node)
+		if (usartdev == usart)
+			break;
+
+	if (usartdev) {
+		list_delete(&usartdev->node);
+		kfree(usartdev);
+	}
+	else
+		ret = -ENOENT;
+
+
+	dev_count--;
+
+	return ret;
+}
+
+int usart_register_master(struct usart_master *usart)
+{
+	char tmp[10] = {0};
+
+	memcpy(tmp, dev_prefix, sizeof(dev_prefix));
+
+	/* XXX: ascii 0 start at 0x30 */
+	tmp[8] = 0x30 + usart->num;
+
+	memcpy(usart->dev.name, tmp, sizeof(tmp));
+
+	list_add_tail(&usart_master_list, &usart->node);
+
+	return 0;
+}
+
+int usart_init(void)
+{
+	int ret = 0;
+	struct usart_bus *bus = NULL;
+
+	bus = (struct usart_bus *)kmalloc(sizeof(struct usart_bus));
+	if (!bus) {
+		error_printk("cannot allocate spi bus\n");
 		return -ENOMEM;
 	}
 
-	usart->num = num;
-	usart->base_reg = base_reg;
-	usart->baud_rate = baud_rate;
+	bus->dev.read = usart_read;
+	bus->dev.write = usart_write;
 
-	dev_prefix[9] = '0';
-	dev_prefix[10] = 0;
-
-	memcpy(usart->dev.name, dev_prefix, 10);
-
-	usart->dev.read = usart_read;
-	usart->dev.write = usart_write;
-
-	ret = device_register(&usart->dev);
+	ret = device_register(&bus->dev);
 	if (ret < 0) {
-		error_printk("failed to register device\n");
+		error_printk("failed to register bus\n");
 		ret = -ENOMEM;
 		goto failed_out;
 	}
 
-	return usart_ops.init(usart);
+	list_initialize(&usart_device_list);
+	list_initialize(&usart_master_list);
+
+	return ret;
 
 failed_out:
-	kfree(usart);
+	kfree(bus);
 	return ret;
 }
+pure_initcall(usart_init);
 
 void usart_print(unsigned char byte)
 {
-	usart_ops.print(usart, byte);
+//	usart_ops.print(usart, byte);
+	return 0;
 }
 
 
 int usart_printl(const char *string)
 {
-	return usart_ops.printl(usart, string);
+//	return usart_ops.printl(usart, string);
+	return 0;
 }
 
 
