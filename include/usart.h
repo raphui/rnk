0660250ba98a547089a4f3f30229b867857544b3
Raphael Poggi <poggi.raph@gmail.com>
2016-10-13 20:49:11 +0200

    drivers: usart: redesign subsystem

--- include/usart.h
+++ include/usart.h
@@ -21,24 +21,46 @@
 
 #include <device.h>
 #include <stdio.h>
+#include <device.h>
 
-struct usart {
+struct usart_master;
+
+struct usart_device {
 	unsigned int num;
-	unsigned int base_reg;
-	unsigned int baud_rate;
+	struct usart_master *master;
 	struct device dev;
+	struct list_node node;
 };
 
 struct usart_operations
 {
-	int (*init)(struct usart *usart);
-	int (*read)(struct usart *usart, unsigned char *buff, unsigned int len);
-	int (*write)(struct usart *usart, unsigned char *buff, unsigned int len);
-	void (*print)(struct usart *usart, unsigned char byte);
-	int (*printl)(struct usart *usart, const char *string);
+	int (*read)(struct usart_master *usart, unsigned char *buff, unsigned int len);
+	int (*write)(struct usart_master *usart, unsigned char *buff, unsigned int len);
+	void (*print)(struct usart_master *usart, unsigned char byte);
+	int (*printl)(struct usart_master *usart, const char *string);
 };
 
-int usart_init(unsigned int num, unsigned int base_reg, unsigned int baud_rate);
+struct usart_master {
+	unsigned int num;
+	unsigned int base_reg;
+	unsigned int source_clk;
+	unsigned int baud_rate;
+	struct list_node node;
+	struct device dev;
+	struct usart_operations usart_ops;
+};
+
+struct usart_bus {
+	struct device dev;
+};
+
+struct usart_device *usart_new_device(void);
+int usart_remove_device(struct usart_device *usart);
+int usart_register_device(struct usart_device *usart);
+struct usart_master *usart_new_master(void);
+int usart_remove_master(struct usart_master *usart);
+int usart_register_master(struct usart_master *usart);
+int usart_init(void);
 
 void usart_print(unsigned char byte);
 int usart_printl(const char *string);
