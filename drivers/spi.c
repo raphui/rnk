8173b64445b26df99adc98afa22a219c8e694493
Raphael Poggi <poggi.raph@gmail.com>
2016-10-17 16:47:37 +0200

    drivers: spi: cleanup include

--- drivers/spi.c
+++ drivers/spi.c
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Frrestore * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <board.h>
+#include <stdio.h>
 #include <spi.h>
 #include <mm.h>
 #include <errno.h>
