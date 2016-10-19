fe95730dd4658d59be3ed19163ac7ace6d3786b3
Raphael Poggi <poggi.raph@gmail.com>
2016-10-19 21:21:57 +0200

    drivers: usart: remove board.h include

--- drivers/usart.c
+++ drivers/usart.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <board.h>
+#include <stdio.h>
 #include <utils.h>
 #include <usart.h>
 #include <mm.h>
