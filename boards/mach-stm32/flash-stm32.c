7a3bbd5fdc37058c5c0ef0b5104f23d273a523d5
Raphael Poggi <poggi.raph@gmail.com>
2016-10-20 13:30:26 +0200

    mach-stm32: flash: fit to new subsys and add fdt support

--- boards/mach-stm32/flash-stm32.c
+++ boards/mach-stm32/flash-stm32.c
@@ -18,6 +18,12 @@
 #include <board.h>
 #include <errno.h>
 #include <stdio.h>
+#include <mm.h>
+#include <string.h>
+#include <init.h>
+#include <device.h>
+#include <fdtparse.h>
+#include <mtd.h>
 
 #define FLASH_PSIZE_BYTE	0x200
 #define CR_PSIZE_MASK		0xFFFFFCFF
@@ -39,15 +45,6 @@ static void stm32_flash_unlock(void)
 		FLASH->KEYR = FLASH_KEY1;
 		FLASH->KEYR = FLASH_KEY2;
 	}
-}
-
-static int stm32_flash_init(struct mtd *mtd)
-{
-	int ret = 0;
-
-	FLASH->SR = SR_ERR_MASK;
-
-	return ret;
 }
 
 static int stm32_flash_wait_operation(void)
@@ -163,8 +160,85 @@ static int stm32_flash_read(struct mtd *mtd, unsigned char *buff, unsigned int s
 }
 
 struct mtd_operations mtd_ops = {
-	.init = stm32_flash_init,
 	.erase = stm32_flash_erase,
 	.write = stm32_flash_write,
 	.read = stm32_flash_read,
 };
+
+static int stm32_flash_of_init(struct mtd *mtd)
+{
+	int offset;
+	int ret = 0;
+	const void *fdt_blob = fdtparse_get_blob();
+
+	offset = fdt_path_offset(fdt_blob, mtd->dev.of_path);
+	if (offset < 0) {
+		ret = -ENOENT;
+		goto out;
+	}
+
+	ret = fdt_node_check_compatible(fdt_blob, offset, mtd->dev.of_compat);
+	if (ret < 0)
+		goto out;
+
+	mtd->base_addr = (unsigned int)fdtparse_get_addr32(offset, "base");
+	if (!mtd->base_addr) {
+		error_printk("failed to retrieve mtd controller base reg from fdt\n");
+		ret = -ENOENT;
+		goto out;
+	}
+
+out:
+	return ret;
+}
+
+static int stm32_flash_init(struct device *dev)
+{
+	int ret = 0;
+	struct mtd *mtd = NULL;
+
+	mtd = mtd_new_controller();
+	if (!mtd) {
+		error_printk("failed to request new mtd controller\n");
+		ret = -EIO;
+		goto err;
+	}
+
+	memcpy(&mtd->dev, dev, sizeof(struct device));
+	mtd->mtd_ops = &mtd_ops;
+
+	ret = stm32_flash_of_init(mtd);
+	if (ret < 0) {
+		error_printk("failed to init mtd controller with fdt data\n");
+		goto err;
+	}
+
+	ret = mtd_register_controller(mtd);
+	if (ret < 0) {
+		error_printk("failed to register stm32 mtd controller\n");
+		goto err;
+	}
+
+	FLASH->SR = SR_ERR_MASK;
+
+	return 0;
+
+err:
+	return ret;
+}
+
+struct device stm32_flash_driver = {
+	.of_compat = "st,stm32f4xx-flash",
+	.probe = stm32_flash_init,
+};
+
+static int stm32_flash_register(void)
+{
+	int ret = 0;
+
+	ret = device_of_register(&stm32_flash_driver);
+	if (ret < 0)
+		error_printk("failed to register stm32_flash device\n");
+	return ret;
+}
+postarch_initcall(stm32_flash_register);
