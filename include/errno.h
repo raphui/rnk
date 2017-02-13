f7533a22a894fafb64ad3fedb7695b3423eec4be
Raphael Poggi <poggi.raph@gmail.com>
2017-02-13 19:59:51 +0100

    include: errno: add somes errno functions

--- include/errno.h
+++ include/errno.h
@@ -155,4 +155,27 @@
 
 #define _LAST_ERRNO	524
 
+
+#define IS_ERR_VALUE(x) (x) >= (unsigned long)-_LAST_ERRNO
+
+static inline void *ERR_PTR(long error)
+{
+	return (void *) error;
+}
+
+static inline long PTR_ERR(const void *ptr)
+{
+	return (long) ptr;
+}
+
+static inline long IS_ERR(const void *ptr)
+{
+	return IS_ERR_VALUE((unsigned long)ptr);
+}
+
+static inline long IS_ERR_OR_NULL(const void *ptr)
+{
+	return !ptr || IS_ERR_VALUE((unsigned long)ptr);
+}
+
 #endif /* ERRNO_H */
