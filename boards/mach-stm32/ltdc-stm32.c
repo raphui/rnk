fec7ea5e44469cf197e3c84a04f512bcb0755488
Raphael Poggi <poggi.raph@gmail.com>
2017-02-16 14:30:40 +0100

    mach-stm32: ltdc: add support for device tree

--- boards/mach-stm32/ltdc-stm32.c
+++ boards/mach-stm32/ltdc-stm32.c
@@ -23,12 +23,18 @@
 #include <mach/pio-stm32.h>
 #include <mach/rcc-stm32.h>
 #include <errno.h>
+#include <device.h>
+#include <init.h>
+#include <string.h>
+#include <fdtparse.h>
 
 #define GCR_MASK		((unsigned int)0x0FFE888F)
 #define RCC_PLLSAIDivR_Div4	((unsigned int)0x00010000)
 
 #define GPIO_AF_LTDC	((unsigned char)0x0E)
 #define GPIO_AF_LCD	((unsigned char)0x09)
+
+struct lcd_operations lcd_ops;
 
 static void stm32_ltdc_pll_sai_config(unsigned int n, unsigned int q, unsigned int r)
 {
@@ -70,25 +76,11 @@ static int stm32_ltdc_enable_fb(struct lcd *ltdc)
 	return ret;
 }
 
-int stm32_ltdc_init(struct lcd *ltdc)
+static int stm32_ltdc_configure(struct lcd *ltdc)
 {
+	int ret = 0;
 	unsigned int h_cycles;
 	unsigned int v_cycles;
-	int ret = 0;
-
-	ret = stm32_rcc_enable_clk(LTDC_BASE);
-	if (ret < 0) {
-		error_printk("cannot enable LTDC periph clock\r\n");
-		return ret;
-	}
-
-	stm32_ltdc_pll_sai_config(127, 7, 5);
-	stm32_ltdc_clk_divconfig(RCC_PLLSAIDivR_Div4);
-
-	RCC->CR |= RCC_CR_PLLSAION;
-
-	while (!(RCC->CR & RCC_CR_PLLSAIRDY))
-		;
 
 	LTDC->GCR &= ~LTDC_GCR_LTDCEN;
 	LTDC->GCR |= LTDC_GCR_DTEN;
@@ -122,8 +114,7 @@ int stm32_ltdc_init(struct lcd *ltdc)
 	ret = stm32_ltdc_enable_fb(ltdc);
 	if (ret < 0) {
 		error_printk("failed to enable framebuffer\r\n");
-		ret = stm32_rcc_disable_clk(LTDC_BASE);
-		return ret;
+		goto out;
 	}
 
 	LTDC->SRCR = LTDC_SRCR_IMR;
@@ -131,10 +122,104 @@ int stm32_ltdc_init(struct lcd *ltdc)
 	/* Enable LCD Controller */
 	LTDC->GCR |= LTDC_GCR_LTDCEN;
 
+out:
 	return ret;
 }
 
-void stm32_ltdc_init_gpio(void)
+static int stm32_ltdc_of_init(struct lcd *ltdc)
+{
+	int offset;
+	int ret = 0;
+	const void *fdt_blob = fdtparse_get_blob();
+
+	offset = fdt_path_offset(fdt_blob, ltdc->dev.of_path);
+	if (offset < 0) {
+		ret = -ENOENT;
+		goto out;
+	}
+
+	ret = fdt_node_check_compatible(fdt_blob, offset, ltdc->dev.of_compat);
+	if (ret < 0)
+		goto out;
+
+	ltdc->base_reg = (unsigned int)fdtparse_get_addr32(offset, "reg");
+	if (!ltdc->base_reg) {
+		error_printk("failed to retrieve ltdc base reg from fdt\n");
+		ret = -ENOENT;
+		goto out;
+	}
+
+//	ret = fdtparse_get_u16_array(offset, "sync", (unsigned short *)&ltdc->hsync, 2 * sizeof(unsigned short));
+//	if (ret < 0)
+//		goto out;
+//
+//	ret = fdtparse_get_u16_array(offset, "porch", (unsigned short *)&ltdc->hbp, 4 * sizeof(unsigned short));
+//	if (ret < 0)
+//		goto out;
+//
+//	ret = fdtparse_get_u32_array(offset, "resolution", (unsigned int *)&ltdc->width, 2 * sizeof(unsigned int));
+//	if (ret < 0)
+//		goto out;
+//
+//	ret = fdtparse_get_int(offset, "bpp", &ltdc->bpp);
+//	if (ret < 0)
+//		goto out;
+
+out:
+	return ret;
+}
+
+static int stm32_ltdc_init(struct device *dev)
+{
+	int ret = 0;
+	struct lcd *ltdc;
+
+	ltdc = lcd_new_device();
+	if (!ltdc) {
+		error_printk("failed to request new lcd device\n");
+		ret = -EIO;
+		goto err;
+	}
+
+	memcpy(&ltdc->dev, dev, sizeof(struct device));
+	ltdc->lcd_ops = &lcd_ops;
+
+	ret = stm32_ltdc_of_init(ltdc);
+	if (ret < 0) {
+		error_printk("failed to init ltdc with fdt data\n");
+		goto err;
+	}
+
+	ret = stm32_rcc_enable_clk(ltdc->base_reg);
+	if (ret < 0) {
+		error_printk("cannot enable LTDC periph clock\r\n");
+		goto err;
+	}
+
+	/* XXX: retrieve all of these from device tree */
+	stm32_ltdc_pll_sai_config(127, 7, 5);
+	stm32_ltdc_clk_divconfig(RCC_PLLSAIDivR_Div4);
+
+	RCC->CR |= RCC_CR_PLLSAION;
+
+	while (!(RCC->CR & RCC_CR_PLLSAIRDY))
+		;
+
+	ret = lcd_register_device(ltdc);
+	if (ret < 0) {
+		error_printk("failed to register ltdc device\n");
+		goto disable_clk;
+	}
+
+	return 0;
+
+disable_clk:
+	stm32_rcc_disable_clk(ltdc->base_reg);
+err:
+	return ret;
+}
+
+static void stm32_ltdc_init_gpio(void)
 {
 	stm32_pio_set_alternate(GPIOA_BASE, 3, GPIO_AF_LTDC);
 	stm32_pio_set_alternate(GPIOA_BASE, 4, GPIO_AF_LTDC);
@@ -172,6 +257,22 @@ void stm32_ltdc_init_gpio(void)
 }
 
 struct lcd_operations lcd_ops = {
-	.init = stm32_ltdc_init,
+	.configure = stm32_ltdc_configure,
 	.init_gpio = stm32_ltdc_init_gpio,
 };
+
+struct device stm32_ltdc_driver = {
+	.of_compat = "st,stm32f4xx-ltdc",
+	.probe = stm32_ltdc_init,
+};
+
+static int stm32_ltdc_register(void)
+{
+	int ret = 0;
+
+	ret = device_of_register(&stm32_ltdc_driver);
+	if (ret < 0)
+		error_printk("failed to register stm32_ltdc device\n");
+	return ret;
+}
+postarch_initcall(stm32_ltdc_register);
