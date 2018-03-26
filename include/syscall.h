24cee9ba1a95adce5fea6a8f043590e8c8bd96f9
Raphael Poggi <poggi.raph@gmail.com>
2018-03-26 14:05:36 +0100

    kernel: add syscall api

---
+++ include/syscall.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2018  Raphaël Poggi <poggi.raph@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SYSCALL_H
+#define SYSCALL_H
+
+enum service_calls {
+	SYSCALL_THREAD_SWITCH,
+	SYSCALL_ACQUIRE_MUTEX,
+	SYSCALL_RELEASE_MUTEX,
+	SYSCALL_WAIT_SEM,
+	SYSCALL_POST_SEM,
+	SYSCALL_USLEEP,
+	SYSCALL_QUEUE_POST,
+	SYSCALL_QUEUE_RECEIVE,
+	SYSCALL_TIMER_ONESHOT,
+	SYSCALL_END,
+};
+
+struct syscall {
+	int number;
+	unsigned int *handler;
+};
+
+extern struct syscall syscall_table[];
+
+int syscall(int number, ...);
+
+#endif /* SYSCALL_H */
---
+++ kernel/syscall.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2018 Raphaël Poggi <poggi.raph@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <stdarg.h>
+#include <errno.h>
+#include <syscall.h>
+#include <scheduler.h>
+#include <mutex.h>
+#include <queue.h>
+#include <semaphore.h>
+#include <time.h>
+
+#include <arch/syscall.h>
+
+struct syscall syscall_table[] = {
+	{SYSCALL_THREAD_SWITCH, (unsigned int *)&schedule_thread},
+	{SYSCALL_ACQUIRE_MUTEX, (unsigned int *)&svc_mutex_lock}, 
+	{SYSCALL_RELEASE_MUTEX, (unsigned int *)&svc_mutex_unlock},
+	{SYSCALL_WAIT_SEM, (unsigned int *)&svc_sem_wait},
+	{SYSCALL_POST_SEM, (unsigned int *)&svc_sem_post},
+	{SYSCALL_USLEEP, (unsigned int *)svc_usleep},
+	{SYSCALL_QUEUE_POST, (unsigned int *)&svc_queue_post},
+	{SYSCALL_QUEUE_RECEIVE, (unsigned int *)&svc_queue_receive},
+	{SYSCALL_TIMER_ONESHOT, (unsigned int *)&svc_timer_soft_oneshot}
+};
+
+int syscall(int number, ...)
+{
+	va_list va;
+	int ret = 0;
+
+	va_start(va, number);
+
+	if (number >= SYSCALL_END)
+		return -EINVAL;
+
+	ret = arch_system_call(number, va);
+
+	return ret;
+}
