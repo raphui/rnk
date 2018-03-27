d43dc2d8378c9bea8d4dd984dc4a51eedda2e765
Raphael Poggi <poggi.raph@gmail.com>
2018-03-27 15:17:36 +0100

    armv7m: kernel: thread: for now user stack has to been executable (svc opcode is generate on the fly)

--- arch/arm/armv7m/kernel/thread.c
+++ arch/arm/armv7m/kernel/thread.c
@@ -90,5 +90,6 @@ void arch_switch_context(struct arch_thread *old, struct arch_thread *new)
 
 	current_ctx_frame = &new->ctx_frame;
 
-	new->mpu.prio = mpu_map_from_high((void *)new->mpu.top_sp, CONFIG_THREAD_STACK_SIZE, MPU_RASR_SHARE_CACHE | MPU_RASR_AP_PRIV_RW_UN_RW | MPU_RASR_XN);
+	//new->mpu.prio = mpu_map_from_high((void *)new->mpu.top_sp, CONFIG_THREAD_STACK_SIZE, MPU_RASR_SHARE_CACHE | MPU_RASR_AP_PRIV_RW_UN_RW | MPU_RASR_XN);
+	new->mpu.prio = mpu_map_from_high((void *)new->mpu.top_sp, CONFIG_THREAD_STACK_SIZE, MPU_RASR_SHARE_CACHE | MPU_RASR_AP_PRIV_RW_UN_RW);
 }
