d95d78b91485bac37312263465d1eb024329fa1c
Raphael Poggi <poggi.raph@gmail.com>
2018-04-13 19:54:08 +0100

    armv7m: mpu: configure mpu for user heap

--- arch/arm/armv7m/mpu.c
+++ arch/arm/armv7m/mpu.c
@@ -93,7 +93,7 @@ int mpu_init(void)
 
 	mpu_map_from_low((void *)CORTEX_M_PERIPH_BASE, SZ_512M, MPU_RASR_DEVICE_SHARE | MPU_RASR_AP_PRIV_RW_UN_NO);
 	mpu_map_from_low(NULL, 32, MPU_RASR_AP_PRIV_NO_UN_NO | MPU_RASR_XN);
-	mpu_map_from_low((void *)CONFIG_KERNEL_HEAP_START, CONFIG_KERNEL_HEAP_END - CONFIG_KERNEL_HEAP_START, MPU_RASR_AP_PRIV_RW_UN_RW);
+	mpu_map_from_low((void *)CONFIG_USER_HEAP_START, CONFIG_USER_HEAP_END - CONFIG_USER_HEAP_START, MPU_RASR_AP_PRIV_RW_UN_RW);
 	mpu_map_from_high((void *)_slibbss, (unsigned int)_elibbss - (unsigned int)_slibbss, MPU_RASR_SHARE_CACHE | MPU_RASR_AP_PRIV_RW_UN_RW);
 	mpu_map_from_high((void *)_slibdata, (unsigned int)_elibdata - (unsigned int)_slibdata, MPU_RASR_SHARE_CACHE | MPU_RASR_AP_PRIV_RW_UN_RW);
 
