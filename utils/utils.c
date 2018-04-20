e5e8b342b97d0a514470009fb919b15e325faa76
Raphael Poggi <poggi.raph@gmail.com>
2018-04-20 12:45:35 +0100

    utils: add next_power_of_2 function

--- utils/utils.c
+++ utils/utils.c
@@ -37,3 +37,15 @@ unsigned int readl(unsigned int reg)
 
 	return val;
 }
+
+unsigned int next_power_of_2(unsigned int n)
+{
+	n--;
+	n |= n >> 1;
+	n |= n >> 2;
+	n |= n >> 4;
+	n |= n >> 8;
+	n |= n >> 16;
+	n++;
+	return n;
+}
