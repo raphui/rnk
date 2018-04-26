8c5380ad9af3516a74e9357198cdd5995ce90674
Raphael Poggi <poggi.raph@gmail.com>
2018-04-26 14:06:17 +0100

    lib: unistd: add some open flags

--- lib/unistd.h
+++ lib/unistd.h
@@ -25,6 +25,11 @@
 #define O_WRONLY        1
 #define O_RDWR          2
 
+#define O_CREAT		0x100
+#define O_EXCL		0x200
+#define O_APPEND	0x1000
+#define O_NONBLOCK	0x2000
+
 int open(const char *path, int flags);
 int close(int fd);
 int write(int fd, const void *buf, size_t size);
