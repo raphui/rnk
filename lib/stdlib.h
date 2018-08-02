cb341131c90d8efe8017e716e774df9d93f974e7
Raphael Poggi <poggi.raph@gmail.com>
2018-08-02 16:29:05 +0100

    lib: stdlib: improve assert macro

--- lib/stdlib.h
+++ lib/stdlib.h
@@ -1,9 +1,10 @@
 #ifndef STDLIB_H
 #define STDLIB_H
 
+#include <stdio.h>
 #include <stddef.h>
 
-#define assert(x) if (!(x)) { for (;;){} }
+#define assert(x) do { if (!(x)) { printf("ASSERT FAILED at (%s:%d): %s\n", __FILE__, __LINE__, #x); for (;;){}; } } while(0)
 
 void *malloc(size_t size);
 void free(void *mem);
