f2169c9931a7b6e4712808f99859933d73ecb012
Raphael Poggi <poggi.raph@gmail.com>
2018-11-29 16:53:29 +0000

    kernel: syscall: add CONFIG ifdef for sem/mutex/queue

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -16,19 +16,25 @@ struct syscall syscall_table[] = {
 	{SYSCALL_THREAD_CREATE, (unsigned int *)&thread_create,		SYSCALL_PRIVILEGE_OPERATION},
 	{SYSCALL_THREAD_JOIN,	(unsigned int *)&thread_join,		SYSCALL_PRIVILEGE_OPERATION},
 	{SYSCALL_THREAD_STOP,	(unsigned int *)&schedule_thread_stop,	SYSCALL_PRIVILEGE_OPERATION},
+#ifdef CONFIG_MUTEX
 	{SYSCALL_MUTEX_CREATE,	(unsigned int *)&kmutex_init,		SYSCALL_PRIVILEGE_OPERATION},
 	{SYSCALL_MUTEX_ACQUIRE, (unsigned int *)&kmutex_lock,		SYSCALL_PRIVILEGE_ELEVATION},
 	{SYSCALL_MUTEX_RELEASE, (unsigned int *)&kmutex_unlock,		SYSCALL_PRIVILEGE_ELEVATION},
+#endif /* CONFIG_MUTEX */
+#ifdef CONFIG_SEMAPHORE
 	{SYSCALL_SEM_CREATE,	(unsigned int *)&ksem_init,		SYSCALL_PRIVILEGE_OPERATION},
 	{SYSCALL_SEM_WAIT,	(unsigned int *)&ksem_wait,		SYSCALL_PRIVILEGE_ELEVATION},
 	{SYSCALL_SEM_POST,	(unsigned int *)&ksem_post,		SYSCALL_PRIVILEGE_ELEVATION},
+#endif /* CONFIG_SEMAPHORE */
 	{SYSCALL_TIME_USLEEP,	(unsigned int *)&ktime_usleep,		SYSCALL_PRIVILEGE_ELEVATION},
 	{SYSCALL_TIME_ONESHOT,	(unsigned int *)&ktime_oneshot,		SYSCALL_PRIVILEGE_OPERATION},
+#ifdef CONFIG_QUEUE
 	{SYSCALL_QUEUE_CREATE,	(unsigned int *)&kqueue_init,		SYSCALL_PRIVILEGE_OPERATION},
 	{SYSCALL_QUEUE_POST,	(unsigned int *)&kqueue_post,		SYSCALL_PRIVILEGE_ELEVATION},
 	{SYSCALL_QUEUE_RECEIVE, (unsigned int *)&kqueue_receive,	SYSCALL_PRIVILEGE_ELEVATION},
 	{SYSCALL_QUEUE_UPDATE,	(unsigned int *)&kqueue_update,		SYSCALL_PRIVILEGE_OPERATION},
 	{SYSCALL_QUEUE_DESTROY, (unsigned int *)&kqueue_destroy,	SYSCALL_PRIVILEGE_OPERATION},
+#endif /* CONFIG_QUEUE */
 	{SYSCALL_FD_OPEN,	(unsigned int *)&svc_open,		SYSCALL_PRIVILEGE_OPERATION},
 	{SYSCALL_FD_CLOSE,	(unsigned int *)&svc_close,		SYSCALL_PRIVILEGE_OPERATION},
 	{SYSCALL_FD_WRITE,	(unsigned int *)&svc_write,		SYSCALL_PRIVILEGE_ELEVATION},
