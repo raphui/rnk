b0c16e6a01017a7b3d7801f3ea83d9af1eccae88
Raphael Poggi <poggi.raph@gmail.com>
2019-01-08 09:28:43 +0000

    utils: symbols: add symbols lookup by hash

--- include/symbols.h
+++ include/symbols.h
@@ -6,7 +6,8 @@ struct symbol {
 	char *name;
 };
 
-extern char *symbol_get_name(unsigned int addr);
-extern int symbol_get_addr(char *name);
+int symbol_get_addr_by_hash(unsigned int hash);
+char *symbol_get_name(unsigned int addr);
+int symbol_get_addr(char *name);
 
 #endif /* SYMBOLS_H */
--- utils/symbols.c
+++ utils/symbols.c
@@ -9,6 +9,33 @@
 extern struct symbol __rnk_ksym_start[];
 extern struct symbol __rnk_ksym_end[];
 
+static unsigned int hash_str(char *str)
+{
+	unsigned int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /*  hash * 33 + c */
+
+	return hash;
+}
+
+int symbol_get_addr_by_hash(unsigned int hash)
+{
+	int ret = -ENXIO;
+	struct symbol *sym;
+
+	for (sym = __rnk_ksym_start; sym < __rnk_ksym_end; sym++) {
+		if (hash_str(sym->name) == hash) {
+			ret = sym->addr;
+			break;
+		}
+	}
+
+	return ret;
+
+}
+
 char *symbol_get_name(unsigned int addr)
 {
 	char *ret = NULL;
