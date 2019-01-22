404e9bff64a0872dab8d64c4b3593a607d7db24b
Raphael Poggi <poggi.raph@gmail.com>
2019-01-22 16:28:43 +0000

    armv7m: systick: use clk driver to retrieve sysclk freq

--- arch/arm/armv7m/systick.c
+++ arch/arm/armv7m/systick.c
@@ -1,19 +1,14 @@
 #include <armv7m/system.h>
+#include <drv/clk.h>
 #include <utils.h>
-
-#ifdef CONFIG_STM32F429
-#define SYS_CLOCK	180000000
-#define SYSTICK_FREQ	1000//4000
-#else
-#define SYS_CLOCK	84000000
-#define SYSTICK_FREQ	1000//4000
-#endif /* CONFIG_STM32F429 */
 
 int systick_init(void)
 {
 	int ret = 0;
+	int sysfreq = clk_get_sysfreq();
+	int systick_freq = 1000;
 
-	writel(SYSTICK_RELOAD, SYS_CLOCK / SYSTICK_FREQ);
+	writel(SYSTICK_RELOAD, sysfreq / systick_freq);
 	writel(SYSTICK_VAL, 0);
 	writel(SYSTICK_CTL, 0x00000007);
 
