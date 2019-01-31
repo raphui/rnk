0c3485e990f22394998a83c3c37b0fad14a42074
Raphael Poggi <poggi.raph@gmail.com>
2019-01-31 10:13:18 +0000

    lib: semaphore: functions should return err code

--- lib/semaphore.c
+++ lib/semaphore.c
@@ -3,20 +3,20 @@
 #include <unistd.h>
 #include <export.h>
 
-void sem_init(sem_t *sem, unsigned int value)
+int sem_init(sem_t *sem, unsigned int value)
 {
-	syscall(SYSCALL_SEM_CREATE, &sem->ksem, value);
+	return syscall(SYSCALL_SEM_CREATE, &sem->ksem, value);
 }
 EXPORT_SYMBOL(sem_init);
 
-void sem_wait(sem_t *sem)
+int sem_wait(sem_t *sem)
 {
-	syscall(SYSCALL_SEM_WAIT, &sem->ksem);
+	return syscall(SYSCALL_SEM_WAIT, &sem->ksem);
 }
 EXPORT_SYMBOL(sem_wait);
 
-void sem_post(sem_t *sem)
+int sem_post(sem_t *sem)
 {
-	syscall(SYSCALL_SEM_POST, &sem->ksem);
+	return syscall(SYSCALL_SEM_POST, &sem->ksem);
 }
 EXPORT_SYMBOL(sem_post);
--- lib/semaphore.h
+++ lib/semaphore.h
@@ -7,8 +7,8 @@ typedef struct sem {
 	struct semaphore ksem;
 } sem_t;
 
-void sem_init(sem_t *sem, unsigned int value);
-void sem_wait(sem_t *sem);
-void sem_post(sem_t *sem);
+int sem_init(sem_t *sem, unsigned int value);
+int sem_wait(sem_t *sem);
+int sem_post(sem_t *sem);
 
 #endif /* SEMAPHORE_H */
