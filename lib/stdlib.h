69cbe2e17189e8781dc99031a84f2187d2eb94a3
Raphael Poggi <poggi.raph@gmail.com>
2019-02-22 21:07:34 +0000

    lib: stdlib: no code at all when config not enable

--- lib/stdlib.h
+++ lib/stdlib.h
@@ -4,7 +4,11 @@
 #include <stdio.h>
 #include <stddef.h>
 
+#ifdef CONFIG_DEBUG
 #define assert(x) do { if (!(x)) { printf("ASSERT FAILED at (%s:%d): %s\n", __FILE__, __LINE__, #x); for (;;){}; } } while(0)
+#else
+#define assert(x)
+#endif
 
 void *malloc(size_t size);
 void free(void *mem);
