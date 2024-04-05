534bb32f91a37fec0c105e6f8465bbabfd14434d
Raphael Poggi <poggi.raph@gmail.com>
2024-04-05 13:34:34 +0100

    mach-stm32: stm32l4xx: initialize correctly the SoC

--- boards/mach-stm32/stm32l4xx.c
+++ boards/mach-stm32/stm32l4xx.c
@@ -7,6 +7,30 @@
 
 void low_level_init(void)
 {
+	/* Set MSION bit */
+	RCC->CR |= RCC_CR_MSION;
+
+	/* Reset CFGR register */
+	RCC->CFGR = 0x00000000;
+
+	/* Reset HSEON, CSSON , HSION, and PLLON bits */
+	RCC->CR &= (uint32_t)0xEAF6FFFF;
+
+	/* Reset PLLCFGR register */
+	RCC->PLLCFGR = 0x00001000;
+
+	/* Reset HSEBYP bit */
+	RCC->CR &= (uint32_t)0xFFFBFFFF;
+
+	/* Disable all interrupts */
+	RCC->CIER = 0x00000000;
+
+	/* Configure the Vector Table location add offset address ------------------*/
+#ifdef VECT_TAB_SRAM
+	writel(SCB_VTOR, SRAM_BASE | VECT_TAB_OFFSET); /* Vector Table Relocation in Internal SRAM */
+#else
+	writel(SCB_VTOR, FLASH_BASE | VECT_TAB_OFFSET); /* Vector Table Relocation in Internal FLASH */
+#endif
 }
 
 int device_init(void)
