69b5382e709984c75030199d9f492eae40d05f0b
Raphael Poggi <poggi.raph@gmail.com>
2025-03-05 16:53:53 +0000

    drivers: console: add support for USB debug output

--- drivers/console.c
+++ drivers/console.c
@@ -11,7 +11,7 @@ int console_write(unsigned char *buff, unsigned int len)
 {
 	int ret = 0;
 
-#ifndef CONFIG_USART_DEBUG
+#if defined(CONFIG_SWO_DEBUG) || defined(CONFIG_SEMIHOSTING_DEBUG)
 	if (!cons || !cons->io_ops)
 		return -ENOTTY;
 
@@ -20,7 +20,19 @@ int console_write(unsigned char *buff, unsigned int len)
 	if (!cons)
 		return -ENOTTY;
 
+#ifdef CONFIG_USART_DEBUG
 	ret = usart_write(&cons->usart->dev, buff, len);
+#elif defined(CONFIG_USB_DEBUG)
+	struct usb_cdc *device = container_of(cons->pdata, struct usb_cdc, dev);
+
+	for (int i = 0; i < len; i++, cons->idx++) {
+		cons->buf[cons->idx % sizeof(cons->buf)] = buff[i];
+		if (buff[i] == '\0') {
+			ret = device->dev.write(cons->pdata, cons->buf, cons->idx % sizeof(cons->buf));
+			cons->idx = 0;
+		}
+	}
+#endif
 #endif
 
 	return ret;
@@ -55,6 +67,24 @@ static int console_init(void)
 	}
 
 	master = container_of(dev, struct usart_master, dev);
+#elif defined(CONFIG_USB_DEBUG)
+	offset = fdtparse_alias_offset("console");
+	if (offset < 0) {
+		error_printk("failed to get offset for alias: console\n");
+		return -ENOENT;
+	}
+
+	path = fdtparse_get_path(offset);
+	if (!path) {
+		error_printk("failed to retrieve console alias path\n");
+		return -ENOENT;
+	}
+
+	dev = device_from_of_path(path);
+	if (!dev) {
+		error_printk("failed to retrieve console device struct\n");
+		return -ENOENT;
+	}
 #endif
 
 	cons = (struct console *)kmalloc(sizeof(struct console));
@@ -79,9 +109,13 @@ static int console_init(void)
 		goto err_register;
 	}
 #endif
+
+#ifdef CONFIG_USB_DEBUG
+	cons->idx = 0;
+#endif
 	cons->pdata = dev;
 
-#ifndef CONFIG_USART_DEBUG
+#if !defined(CONFIG_USART_DEBUG) && !defined(CONFIG_USB_DEBUG)
 	cons->io_ops = &io_op;
 #endif
 
@@ -96,7 +130,7 @@ err_usart:
 	return ret;
 #endif
 }
-#if defined(CONFIG_SWO_DEBUG) || defined(CONFIG_SEMIHOSTING_DEBUG)
+#if !defined(CONFIG_USART_DEBUG) && !defined(CONFIG_USB_DEBUG)
 core_initcall(console_init);
 #else
 late_initcall(console_init);
