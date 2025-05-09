5171920a19f94b21e62cad3cab093a95c2bce849
Raphael Poggi <poggi.raph@gmail.com>
2025-05-09 14:57:30 +0100

    kernel: syscall: add SYSCALL_PIO_[ENABLE/DISABLE]_IRQ

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -52,5 +52,7 @@ struct syscall syscall_table[] = {
 	{SYSCALL_PIO_SET_STATE, (unsigned int *)pio_set_state,		SYSCALL_PRIVILEGE_ELEVATION},
 	{SYSCALL_PIO_GET_STATE, (unsigned int *)pio_get_state,		SYSCALL_PRIVILEGE_ELEVATION},
 	{SYSCALL_PIO_REQUEST_IRQ, (unsigned int *)pio_request_interrupt, SYSCALL_PRIVILEGE_OPERATION},
-	{SYSCALL_PRINT,		(unsigned int *)__printk,		SYSCALL_PRIVILEGE_OPERATION},
+	{SYSCALL_PIO_ENABLE_IRQ, (unsigned int *)pio_enable_interrupt, SYSCALL_PRIVILEGE_OPERATION},
+	{SYSCALL_PIO_DISABLE_IRQ, (unsigned int *)pio_disable_interrupt, SYSCALL_PRIVILEGE_OPERATION},
+	{SYSCALL_PRINT,		(unsigned int *)__printk,		SYSCALL_PRIVILEGE_ELEVATION},
 };
