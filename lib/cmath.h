7e891940c196ab533a645b5919d9fca05763a905
Raphael Poggi <raphael.poggi@lynxleap.co.uk>
2026-01-09 20:52:14 +0000

    lib: add floating point functions

---
+++ lib/cmath.h
@@ -0,0 +1,12 @@
+#ifndef CMATH_H
+#define CMATH_H
+
+float exp2f(float x);
+float log2f(float x);
+float exp10f(float x);
+float log10f(float x);
+float powf(float x, float y);
+float sqrtf(float x);
+float fabs(float x);
+
+#endif /* CMATH_H */
